8ace9e157728bf4d3926c50c908a2fa801424e4c
neige-i <ibrahim.naji.rachdi@gmail.com>
2022-01-19 11:54:17 +0100

    Add configuration for w600dp portrait

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailActivity.kt
@@ -1,12 +1,16 @@
 package com.openclassrooms.realestatemanager.ui.detail
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.ActivityDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
+
+    private val viewModel: DetailParentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,13 @@ class DetailActivity : AppCompatActivity() {
         setSupportActionBar(binding.detailToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         title = "Detail"
+
+        viewModel.endActivityEvent.observe(this) { finish() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onActivityResumed(resources.getBoolean(R.bool.is_tablet))
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailParentViewModel.kt
@@ -0,0 +1,21 @@
+package com.openclassrooms.realestatemanager.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.openclassrooms.realestatemanager.ui.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailParentViewModel @Inject constructor() : ViewModel() {
+
+    private val endActivityMutableEvent = SingleLiveEvent<Unit>()
+    val endActivityEvent: LiveData<Unit> = endActivityMutableEvent
+
+    fun onActivityResumed(isTablet: Boolean) {
+        // Do not display DetailActivity in tablet configuration
+        if (isTablet) {
+            endActivityMutableEvent.call()
+        }
+    }
+}
