d37cfb992e1dd399143b93a39990a7f258e7b0ae
neige-i <ibrahim.naji.rachdi@gmail.com>
2022-04-27 15:32:02 +0200

    Rename/refactor detail feature (add static map)

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailFragment.kt
@@ -6,9 +6,12 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.FragmentDetailBinding
+import com.openclassrooms.realestatemanager.ui.detail.DetailViewState.Empty
+import com.openclassrooms.realestatemanager.ui.detail.DetailViewState.WithInfo
 import com.openclassrooms.realestatemanager.ui.util.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,45 +25,51 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
         val viewModel: DetailViewModel by viewModels()
 
-        val mediaAdapter = MediaAdapter {
-            viewModel.onMediaClicked(it)
-        }
-        binding.detailMediaList.adapter = mediaAdapter
+        val photoAdapter = PhotoAdapter { selectedPhoto -> viewModel.onPhotoClicked(selectedPhoto) }
+        binding.detailPhotoList.adapter = photoAdapter
 
         val chipAdapter = ChipAdapter()
         binding.detailPoiList.adapter = chipAdapter
         binding.detailPoiList.layoutManager = FlexboxLayoutManager(requireContext())
 
-        viewModel.viewState.observe(viewLifecycleOwner) {
-            binding.detailNoSelectionLbl.isVisible = it.isNoSelectionLabelVisible
+        viewModel.viewStateLiveData.observe(viewLifecycleOwner) { detailViewState ->
+            binding.detailNoSelectionLbl.isVisible = detailViewState.isNoSelectionLabelVisible
 
-            when (it) {
-                is DetailViewState.Empty -> {
-                    binding.detailNoSelectionLbl.text = it.noSelectionLabelText
+            when (detailViewState) {
+                is Empty -> {
+                    binding.detailNoSelectionLbl.text = detailViewState.noSelectionLabelText
                 }
-                is DetailViewState.Info -> {
-                    binding.detailAvailabilityLbl.text = it.saleText
-                    binding.detailAvailabilityLbl.backgroundTintList =
+                is WithInfo -> {
+                    binding.detailTypeTxt.text = detailViewState.type
+                    binding.detailTypeTxt.isVisible = detailViewState.areTypeAndPriceVisible
+                    binding.detailPriceTxt.text = detailViewState.price
+                    binding.detailPriceTxt.isVisible = detailViewState.areTypeAndPriceVisible
+
+                    binding.detailAvailabilityTxt.text = detailViewState.saleText
+                    binding.detailAvailabilityTxt.backgroundTintList =
                         ContextCompat.getColorStateList(
                             requireContext(),
-                            it.saleBackgroundColor
+                            detailViewState.saleBackgroundColor
                         )
 
-                    binding.detailDescriptionTxt.text = it.description
+                    binding.detailDescriptionTxt.text = detailViewState.description
 
-                    mediaAdapter.submitList(it.photoList)
+                    photoAdapter.submitList(detailViewState.photoList)
 
-                    binding.detailSurfaceTxt.text = it.surface
-                    binding.detailRoomTxt.text = it.roomCount
-                    binding.detailBathTxt.text = it.bathroomCount
-                    binding.detailBedTxt.text = it.bedroomCount
+                    binding.detailSurfaceTxt.text = detailViewState.surface
+                    binding.detailRoomTxt.text = detailViewState.roomCount
+                    binding.detailBathTxt.text = detailViewState.bathroomCount
+                    binding.detailBedTxt.text = detailViewState.bedroomCount
 
-                    binding.detailLocationTxt.text = it.address
-                    chipAdapter.submitList(it.poiList)
+                    binding.detailLocationTxt.text = detailViewState.address
+                    chipAdapter.submitList(detailViewState.poiList)
+                    Glide.with(binding.root)
+                        .load(detailViewState.mapUrl)
+                        .into(binding.detailMapImg)
 
-                    binding.detailMarketTxt.text = it.market_dates
+                    binding.detailMarketTxt.text = detailViewState.market_dates
 
-                    binding.detailAgentTxt.text = it.agentName
+                    binding.detailAgentTxt.text = detailViewState.agentName
                 }
             }
         }
